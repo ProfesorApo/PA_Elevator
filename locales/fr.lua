4478be0225c0dfe1d1406f72efc5fe20e75f582a
Profesor Apo <ruffault.alex35@gmail.com>
2024-09-19 06:18:13 +0200

    Create fr.lua

---
+++ locales/fr.lua
@@ -0,0 +1,10 @@
+Locales = Locales or {}
+
+Locales['fr'] = {
+    use_elevator = "Utiliser l'ascenseur",
+    select_floor = "Sélectionner un étage",
+    floor_invalid = "Données de l'étage invalides. Veuillez réessayer.",
+    arrived_at = "Vous êtes arrivé à ",
+    missing_elevator = "Nom de l'ascenseur non défini. Veuillez réessayer.",
+    no_floors_found = "Aucun étage trouvé pour l'ascenseur ",
+}
