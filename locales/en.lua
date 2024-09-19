134cd89a19682c67b70cb8c6d8445d91b614cfa0
Profesor Apo <ruffault.alex35@gmail.com>
2024-09-19 06:18:32 +0200

    Create en.lua

---
+++ locales/en.lua
@@ -0,0 +1,10 @@
+Locales = Locales or {}
+
+Locales['en'] = {
+    use_elevator = "Use Elevator",
+    select_floor = "Select Floor",
+    floor_invalid = "Invalid floor data. Please try again.",
+    arrived_at = "You have arrived at ",
+    missing_elevator = "Elevator name is missing. Please try again.",
+    no_floors_found = "No floors found for the elevator ",
+}
